ec2a7b9a54e7a98a90e84789dba7bbb2a9a9403a
Dobbymin <gmin0701@knu.ac.kr>
2024-04-07 01:01:15 +0900

    feat: schema setting

---
+++ backend/Models/user.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'User must type name'],
+        unique: true,
+    },
+    token: {
+        type: String,
+    },
+    online: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+export default model('User', userSchema);
